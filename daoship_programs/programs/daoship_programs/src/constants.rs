1e091140ba986dc787fec1573d0174061aa9737e
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-07-23 15:28:05 +0530

    constants: Added DAOSHIP_ADMIN_PUBKEY

--- daoship_programs/programs/daoship_programs/src/constants.rs
+++ daoship_programs/programs/daoship_programs/src/constants.rs
@@ -4,3 +4,4 @@ pub const PUBKEY_LENGTH: usize = 32;
 pub const IMAGE_LINK_LENGTH: usize = 50;
 pub const DATA_LENGTH: usize = 8;
 pub const BOOL_LENGTH: usize = 1;
+pub const DAOSHIP_ADMIN_PUBKEY: &str = "FdkvKDmH8ikxmFcq9sZdnQYy1YZ7A5DJLQTmTGFmbAVq";
