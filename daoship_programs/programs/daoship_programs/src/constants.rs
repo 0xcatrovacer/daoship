379d5fb74dcd5341fc5f014f346b69defe3c6675
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-07-23 12:08:27 +0530

    restructure: Shifted constants to separate file

---
+++ daoship_programs/programs/daoship_programs/src/constants.rs
@@ -0,0 +1,6 @@
+pub const DISCRIMINATOR_LENGTH: usize = 8;
+pub const NAME_LENGTH: usize = 30 * 4;
+pub const PUBKEY_LENGTH: usize = 32;
+pub const IMAGE_LINK_LENGTH: usize = 50;
+pub const DATA_LENGTH: usize = 8;
+pub const BOOL_LENGTH: usize = 1;
