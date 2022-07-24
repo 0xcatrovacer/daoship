64360153f7d3e7aa9fd5553f016d39520cc3808e
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-07-24 11:51:33 +0530

    feat: Added state Bounty

---
+++ daoship_programs/programs/daoship_programs/src/state/bounty.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+
+use crate::constants::*;
+
+#[account]
+pub struct Bounty {
+    /// The project initializing the Job
+    pub project: Pubkey,
+
+    /// The dao where project is shown
+    pub dao: Pubkey,
+
+    /// Bounty Vault Mint
+    pub bounty_vault_mint: Pubkey,
+
+    /// Bounty Token Account
+    pub bounty_vault_account: Pubkey,
+
+    /// Timestamp when Job was posted
+    pub post_ts: i64,
+
+    /// Number of active applications
+    pub applications: u64,
+
+    /// Number of approved applicants
+    pub approved: u64,
+
+    /// Description of the job
+    pub bounty_description: String,
+
+    /// Bump
+    pub bump: u8,
+}
+
+impl Bounty {
+    pub const LEN: usize = DISCRIMINATOR_LENGTH // 8-byte discriminator
+        + PUBKEY_LENGTH                         // Project Pubkey
+        + PUBKEY_LENGTH                         // DAO Pubkey
+        + PUBKEY_LENGTH                         // Mint of Bounty Vault Token
+        + PUBKEY_LENGTH                         // Vault Account for Bounty
+        + DATA_LENGTH                           // Timestamp for Bounty post
+        + DATA_LENGTH                           // Number of applicants
+        + DATA_LENGTH                           // Number of approved users
+        + LINK_LENGTH                           // Description of Bounty
+        + BOOL_LENGTH; // Bump
+}
--- daoship_programs/programs/daoship_programs/src/state/mod.rs
+++ daoship_programs/programs/daoship_programs/src/state/mod.rs
@@ -1,8 +1,10 @@
+pub mod bounty;
 pub mod dao;
 pub mod job;
 pub mod project;
 pub mod project_whitelist;
 
+pub use bounty::*;
 pub use dao::*;
 pub use job::*;
 pub use project::*;
