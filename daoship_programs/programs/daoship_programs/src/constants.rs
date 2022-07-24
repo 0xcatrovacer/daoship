dfc7f9630042f9c0802e31c214edc22f2d7fb2b9
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-07-24 13:42:20 +0530

    feat: Added constants to handle reputation

--- daoship_programs/programs/daoship_programs/src/constants.rs
+++ daoship_programs/programs/daoship_programs/src/constants.rs
@@ -5,3 +5,12 @@ pub const LINK_LENGTH: usize = 50 * 4;
 pub const DATA_LENGTH: usize = 8;
 pub const BOOL_LENGTH: usize = 1;
 pub const DAOSHIP_ADMIN_PUBKEY: &str = "FdkvKDmH8ikxmFcq9sZdnQYy1YZ7A5DJLQTmTGFmbAVq";
+
+pub const CREATE_JOB_REP: i64 = 3;
+pub const HIRE_APPLICANT_REP: i64 = 20;
+pub const CLOSE_JOB_REP: i64 = -5;
+pub const CREATE_BOUNTY_REP: i64 = 1;
+pub const ACCEPT_BOUNTY_REP: i64 = 15;
+pub const CLOSE_BOUNTY_REP: i64 = -2;
+pub const COMPLETE_BOUNTY_REP: i64 = 20;
+pub const GET_HIRED_REP: i64 = 30;
