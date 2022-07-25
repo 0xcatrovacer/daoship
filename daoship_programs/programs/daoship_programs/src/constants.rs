90067f4dda9d8abcc0c66cbbf6eb9d7a81bbf9e0
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-07-26 00:27:03 +0530

    feat: Added constant ENUM_LENGTH

--- daoship_programs/programs/daoship_programs/src/constants.rs
+++ daoship_programs/programs/daoship_programs/src/constants.rs
@@ -3,6 +3,7 @@ pub const NAME_LENGTH: usize = 30 * 4;
 pub const PUBKEY_LENGTH: usize = 32;
 pub const LINK_LENGTH: usize = 50 * 4;
 pub const BIO_LENGTH: usize = 50 * 4;
+pub const ENUM_LENGTH: usize = 50 * 4;
 pub const DATA_LENGTH: usize = 8;
 pub const BOOL_LENGTH: usize = 1;
 pub const DAOSHIP_ADMIN_PUBKEY: &str = "FdkvKDmH8ikxmFcq9sZdnQYy1YZ7A5DJLQTmTGFmbAVq";
