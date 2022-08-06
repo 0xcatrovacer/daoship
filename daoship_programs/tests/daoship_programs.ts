c60e474eed274be0c8f976dc6166ebfb93de0fd7
0xcatrovacer <swarnabgarang@gmail.com>
2022-08-06 22:08:23 +0530

    testing: Added test Can initialize a Project

--- daoship_programs/tests/daoship_programs.ts
+++ daoship_programs/tests/daoship_programs.ts
@@ -11,26 +11,35 @@ describe("daoship_programs", () => {
 
   const program = anchor.workspace.DaoshipPrograms as Program<DaoshipPrograms>;
   
-  let usdcMint = null;
+  let usdcMint: PublicKey = null;
   let mintAuthority: Keypair = null;
 
   let dao: PublicKey = null;
   let daoAuthority: Keypair = null;
   let daoUsdcTokenAccount = null;
 
+  let project: PublicKey = null;
+  let projectAuthority: Keypair = null;
+  let projectUsdcTokenAccount: PublicKey = null;
+
   it("Can initialize the state of the program", async () => {
     mintAuthority = anchor.web3.Keypair.generate();
     daoAuthority = anchor.web3.Keypair.generate();
+    projectAuthority = anchor.web3.Keypair.generate();
 
     const fundMintAuthority = await program.provider.connection.requestAirdrop(
       mintAuthority.publicKey, 10 * LAMPORTS_PER_SOL
     );
     const fundDaoAuthority = await program.provider.connection.requestAirdrop(
       daoAuthority.publicKey, 5 * LAMPORTS_PER_SOL
-    )
+    );
+    const fundProjectAuthority = await program.provider.connection.requestAirdrop(
+      projectAuthority.publicKey, 5 * LAMPORTS_PER_SOL
+    );
 
     await program.provider.connection.confirmTransaction(fundMintAuthority);
     await program.provider.connection.confirmTransaction(fundDaoAuthority);
+    await program.provider.connection.confirmTransaction(fundProjectAuthority);
 
     usdcMint = await createMint(
       provider.connection,
@@ -45,6 +54,13 @@ describe("daoship_programs", () => {
       daoAuthority,
       usdcMint,
       daoAuthority.publicKey,
+    )
+
+    projectUsdcTokenAccount = await createAccount(
+      provider.connection,
+      projectAuthority,
+      usdcMint,
+      projectAuthority.publicKey,
     )
 
   });
@@ -95,5 +111,37 @@ describe("daoship_programs", () => {
     const whitelistedDao = await program.account.dao.fetch(dao);
 
     assert.strictEqual(whitelistedDao.isWhitelisted, true);
+  })
+
+  it("Can initialize a Project", async () => {
+    const [_project, _projectBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('project'),
+        projectAuthority.publicKey.toBuffer(),
+      ],
+      program.programId,
+    );
+
+    project = _project;
+
+    await program.methods.initProject('LendProtocol', 'https://assets.ledprotocol.io')
+      .accounts({
+        project: project,
+        authority: projectAuthority.publicKey,
+        projectVaultMint: usdcMint,
+        projectVaultTokenAccount: projectUsdcTokenAccount,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([projectAuthority])
+      .rpc();
+
+    const createdProject = await program.account.project.fetch(project);
+
+    assert.strictEqual(createdProject.name, 'LendProtocol');
+    assert.strictEqual(createdProject.imgLink, 'https://assets.ledprotocol.io');
+    assert.strictEqual(createdProject.authority.toBase58(), projectAuthority.publicKey.toBase58());
+    assert.strictEqual(createdProject.projectVault.toBase58(), projectUsdcTokenAccount.toBase58());
+    assert.strictEqual(createdProject.vaultMint.toBase58(), usdcMint.toBase58());
+    assert.strictEqual(createdProject.bump, _projectBump);
   })
 });
