f680e39f6d0b522c6679190ace3d828f4fa35664
0xcatrovacer <swarnabgarang@gmail.com>
2022-08-09 04:59:53 +0530

    feat: Added connect wallet button on Landing

--- daoship_client/src/pages/LandingPage/LandingPage.tsx
+++ daoship_client/src/pages/LandingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import './LandingPage.css'
 
 function LandingPage() {
@@ -7,6 +8,12 @@ function LandingPage() {
         <div className="landing__herotext">GET ACCESS TO</div>
         <div className="landing__herotext">BOUNTIES AND JOBS</div>
         <div className="landing__herotext">ON SOLANA</div>
+      </div>
+
+      <div className="landing__connectbuttoncont">
+        <WalletModalProvider>
+          <WalletMultiButton className='landing__connectbutton' />
+        </WalletModalProvider>
       </div>
     </div>
   )
